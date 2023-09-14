c11f4c02fdb610ff237f3e004bae3f39e4bc783f
chenghsienyu <chenghsienyu@aim.com>
2023-09-15 03:44:43 +0800

    update files

---
+++ shot.py
@@ -0,0 +1,8 @@
+import supervision as sv
+import cv2
+
+generator = sv.get_video_frames_generator("./mall.mp4")
+iterator = iter(generator)
+frame = next(iterator)
+
+cv2.imwrite("frame.jpg", frame)
